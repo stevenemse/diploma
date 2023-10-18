872377dfefbd746d01261ca33a7bfac0c9fa03b9
samuel-e_m <samuelminkada5@gmail.com>
2023-10-18 11:41:18 +0100

    add goodlink

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-import AuthDetails from './components/AuthDetails';
+import AuthDetails from './components/auth/AuthDetails';
 
 function App() {
   return (
